6c7c05ba87d24b8f34bc56bcad24a46c9ea5c5f4
karthik-v-dev <karthikvoorugonda@gmail.com>
2020-10-11 15:39:30 +0530

    Updates

---
+++ precache-manifest.00fb1ad963fdac02d0b67f5e90cf1d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4c15dd0c0d78704b4005e499306b85",
+    "url": "/.github.io/TODO-ADD-ITEMS/index.html"
+  },
+  {
+    "revision": "7dabcf3fa307b2147184",
+    "url": "/.github.io/TODO-ADD-ITEMS/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9653048a15b184c98bbf",
+    "url": "/.github.io/TODO-ADD-ITEMS/static/js/2.d060ad9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/.github.io/TODO-ADD-ITEMS/static/js/2.d060ad9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dabcf3fa307b2147184",
+    "url": "/.github.io/TODO-ADD-ITEMS/static/js/main.bca6cdbf.chunk.js"
+  },
+  {
+    "revision": "7e0a4e7cab41484f3463",
+    "url": "/.github.io/TODO-ADD-ITEMS/static/js/runtime-main.1a226251.js"
+  }
+]);
